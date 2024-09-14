9fe6c28d9a5a8e1d8732f0055c2d55badea89c0b
ilya.ignatev <ilya.ignatev@onlinesup.com>
2024-09-14 14:42:53 +0300

    Compatibility date

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    devtools: {enabled: true},
-    extends : ['@nuxt/ui-pro'],
-    modules : ["@nuxt/ui"]
+  devtools: {enabled: true},
+  extends : ['@nuxt/ui-pro'],
+  modules : ["@nuxt/ui"],
+  compatibilityDate: "2024-09-14"
 })
