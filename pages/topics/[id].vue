4e1e7a09769c0bbee6b3bac47d8b88717fe17e63
ilya.ignatev <ilya.ignatev@onlinesup.com>
2024-09-16 02:20:27 +0300

    Comments and layout

--- pages/topics/[id].vue
+++ pages/topics/[id].vue
@@ -5,6 +5,23 @@ const route = useRoute();
 const repo  = new TopicRepository();
 
 const {data: topic} = await repo.show(parseInt(route.params.id as string));
+
+const topAnchor      = ref();
+const commentsAnchor = ref();
+
+const {y: topY}      = useElementBounding(topAnchor);
+const {y: commentsY} = useElementBounding(commentsAnchor);
+
+const links = computed(() => [{
+    label : 'Topic',
+    to    : '#top',
+    active: topY.value <= 0 && commentsY.value > 0
+}, {
+    label : 'Comments',
+    to    : '#comments',
+    active: commentsY.value <= 0,
+    badge : topic.value.comments.length
+}]);
 </script>
 
 <template>
@@ -19,8 +36,28 @@ const {data: topic} = await repo.show(parseInt(route.params.id as string));
             </template>
         </UHeader>
 
-        <UContainer class="py-5">
-            <ElementRenderer v-for="element in topic.description" v-bind="element"/>
+        <UContainer class="flex gap-5 py-5">
+            <UCard class="w-[200px] shrink-0 sticky top-[5.225rem] h-full">
+                <UVerticalNavigation :links="links"/>
+            </UCard>
+
+            <div id="top" ref="topAnchor" class="absolute top-0"></div>
+
+            <div class="flex flex-col gap-5 grow">
+                <UCard>
+                    <ElementRenderer v-for="element in topic.description" v-bind="element"/>
+                </UCard>
+
+                <div class="flex flex-col gap-5 relative">
+                    <div id="comments" ref="commentsAnchor" class="absolute -top-20"></div>
+
+                    <h3 class="text-xl font-semibold">Комментарии</h3>
+
+                    <UCard v-for="comment in topic.comments">
+                        <ElementRenderer v-for="element in comment" v-bind="element"/>
+                    </UCard>
+                </div>
+            </div>
         </UContainer>
     </div>
 </template>
