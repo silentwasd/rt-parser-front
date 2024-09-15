18c9896bd36bc0cf4bf7c2f4b4717d4bde1ce147
ilya.ignatev <ilya.ignatev@onlinesup.com>
2024-09-16 00:37:50 +0300

    Table for search results

--- pages/index.vue
+++ pages/index.vue
@@ -11,16 +11,36 @@ const {data: topics, status} = await repo.search(() => ({query: queryDeb.value})
 
 watch(queryDeb, value => {
     router.push({
-        path: '/',
+        path : '/',
         query: {query: value}
     });
 });
+
+const columns = [{
+    key  : 'name',
+    label: 'Name'
+}, {
+    key  : 'category',
+    label: 'Category'
+}, {
+    key  : 'size',
+    label: 'Size'
+}, {
+    key  : 'seeds',
+    label: 'Seeds'
+}, {
+    key  : 'leeches',
+    label: 'Leeches'
+}, {
+    key  : 'downloads',
+    label: 'Downloads'
+}];
 </script>
 
 <template>
     <div class="flex flex-col items-center h-dvh overflow-auto"
          :class="{'justify-center': !topics}">
-        <div class="w-full p-5 sticky top-0 bg-white dark:bg-gray-900">
+        <div class="w-full p-5 sticky top-0 bg-white dark:bg-gray-900 z-10">
             <div class="w-full max-w-[600px] mx-auto flex gap-2.5">
                 <UInput v-model="query"
                         placeholder="Search..."
@@ -36,37 +56,16 @@ watch(queryDeb, value => {
             </div>
         </div>
 
-        <div class="w-full flex flex-col gap-5 md:gap-2.5 pb-5">
-            <NuxtLink v-for="topic in topics" :key="topic.id"
-                      class="flex flex-col md:flex-row gap-2.5 px-5"
-                      :to="`/topics/${topic.id}`">
-                <div class="bg-gray-100 dark:bg-gray-800 border-2 dark:border-gray-700 rounded-md grow px-2.5 py-2 truncate">
-                    <p class="truncate" :title="topic.name">{{ topic.name }}</p>
-                </div>
+        <div v-if="topics" class="w-full flex flex-col gap-5 md:gap-2.5">
+            <UTable :columns="columns" :rows="topics" @select="navigateTo(`/topics/${$event.id}`)">
+                <template #name-data="{row}">
+                    <p class="truncate w-[600px]" :title="row.name">{{ row.name }}</p>
+                </template>
 
-                <div class="shrink-0 flex gap-2.5">
-                    <div class="grow bg-gray-600 dark:bg-gray-950 border-2 border-gray-800 dark:border-gray-950 rounded-md px-2.5 py-2 text-center w-24 text-gray-50 font-semibold">
-                        <FileSize :size="topic.size"/>
-                    </div>
-
-                    <div class="grow bg-emerald-400 dark:bg-emerald-900 border-2 border-emerald-500 dark:border-emerald-700 rounded-md px-2.5 py-2 text-center w-16 font-semibold">
-                        {{ topic.seeds }}
-                    </div>
-
-                    <div class="grow bg-red-400 dark:bg-red-900 border-2 border-red-500  dark:border-red-700 rounded-md px-2.5 py-2 text-center w-16 font-semibold">
-                        {{ topic.leeches }}
-                    </div>
-
-                    <div class="grow bg-fuchsia-400 dark:bg-fuchsia-900 border-2 border-fuchsia-500  dark:border-fuchsia-700 rounded-md px-2.5 py-2 text-center w-20 font-semibold">
-                        {{ topic.downloads }}
-                    </div>
-
-                    <div class="hidden xl:block grow bg-orange-400 dark:bg-orange-900 border-2 border-orange-500  dark:border-orange-700 rounded-md px-2.5 py-2 text-center w-36 font-semibold truncate"
-                         :title="topic.author">
-                        {{ topic.author }}
-                    </div>
-                </div>
-            </NuxtLink>
+                <template #size-data="{row}">
+                    <FileSize :size="row.size"/>
+                </template>
+            </UTable>
         </div>
     </div>
 </template>
