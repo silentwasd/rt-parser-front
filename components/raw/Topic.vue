7b7f83c99dd3cab159f5e0a5d4b4dce1ed4e68d4
BashkirskyKot <silentwasd@gmail.com>
2026-02-04 20:17:41 +0300

    Designed topic

--- components/raw/Topic.vue
+++ components/raw/Topic.vue
@@ -5,29 +5,48 @@ const props = defineProps<{
 </script>
 
 <template>
-    <NuxtLink class="block bg-gray-800 rounded px-2.5 py-1.5" :to="`/topics/${props.topic.id}`">
-        <p>{{ topic.title }}</p>
+    <NuxtLink
+        class="block bg-gray-800 hover:bg-gray-750 rounded-lg px-4 py-3 border border-gray-700 hover:border-gray-600"
+        :to="`/topics/${props.topic.id}`"
+    >
+        <h3 class="font-medium mb-2 text-gray-100 line-clamp-2 leading-snug">
+            {{ topic.title }}
+        </h3>
 
-        <div class="flex gap-5">
-            <div>
-                <p v-if="topic.author">Автор: {{ topic.author.name }}</p>
-                <p v-else>Автор: Гость</p>
+        <div class="flex flex-wrap gap-x-6 gap-y-2 text-sm">
+            <div class="flex items-center gap-1">
+                <UIcon name="i-mdi-account" class="text-gray-400 size-5" />
+                <span class="text-gray-300 font-semibold">
+                    {{ topic.author ? topic.author.name : 'Гость' }}
+                </span>
             </div>
 
-            <div>
-                <p>Размер: <FileSize :size="topic.size"/></p>
+            <div class="flex items-center gap-1">
+                <UIcon name="i-mdi-harddisk" class="text-gray-400 size-5" />
+                <span class="text-gray-300 font-semibold">
+                    <FileSize :size="topic.size"/>
+                </span>
             </div>
 
-            <div>
-                <p>Сидеры: {{ topic.seeders }}</p>
+            <div class="flex items-center gap-1">
+                <UIcon name="i-mdi-upload" class="text-green-400 size-5" />
+                <span class="text-green-400 font-semibold">
+                    {{ topic.seeders }}
+                </span>
             </div>
 
-            <div>
-                <p>Личеры: {{ topic.leechers }}</p>
+            <div class="flex items-center gap-1">
+                <UIcon name="i-mdi-download" class="text-red-400 size-5" />
+                <span class="text-red-400 font-semibold">
+                    {{ topic.leechers }}
+                </span>
             </div>
 
-            <div>
-                <p>Скачиваний: {{ topic.downloads }}</p>
+            <div class="flex items-center gap-1">
+                <UIcon name="i-mdi-download-box" class="text-gray-400 size-5" />
+                <span class="text-gray-300 font-semibold">
+                    {{ topic.downloads }}
+                </span>
             </div>
         </div>
     </NuxtLink>
