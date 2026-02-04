45be6fe57d624ac54425a4ee1b84a705a5661048
BashkirskyKot <silentwasd@gmail.com>
2026-02-04 21:05:32 +0300

    Pagination

--- pages/raw/categories/[category].vue
+++ pages/raw/categories/[category].vue
@@ -1,12 +1,28 @@
 <script setup lang="ts">
-const route = useRoute();
+const route  = useRoute();
+const router = useRouter();
+
+const page = computed({
+    get: () => {
+        const pageParam = route.query.page;
+        return pageParam ? parseInt(pageParam as string, 10) : 1;
+    },
+    set: (newPage: number) => {
+        router.replace({
+            query: {...route.query, page: newPage.toString()}
+        });
+        goToPage.value = newPage;
+    }
+});
+
+const goToPage = ref<number>(page.value);
 
 const {data: currentCategory} = await useFetch(`/raw/categories/${route.params.category}`, fetchOptions({
-    transform: r => r.data
+    query: {page}
 }));
 
 useSeoMeta({
-    title: currentCategory.value?.title ?? 'Неизвестная категория'
+    title: currentCategory.value?.data.title ?? 'Неизвестная категория'
 });
 </script>
 
@@ -14,12 +30,24 @@ useSeoMeta({
     <UContainer class="py-5 h-dvh">
         <div class="flex gap-5 h-full">
             <div class="flex flex-col gap-2.5 overflow-auto shrink-0 w-64">
-                <RawCategory :category="{id: currentCategory.parent_id, title: 'Назад'}"/>
-                <RawCategory v-for="category in currentCategory.categories" :category="category"/>
+                <RawCategory :category="{id: currentCategory.data.parent_id, title: 'Назад'}"/>
+                <RawCategory v-for="category in currentCategory.data.categories"
+                             :category="category"
+                             :active="category.id === currentCategory.data.id"/>
             </div>
 
-            <div v-if="currentCategory.topics.length > 0" class="flex flex-col gap-2.5 overflow-auto grow w-0">
-                <RawTopic v-for="topic in currentCategory.topics" :topic="topic"/>
+            <div class="flex flex-col gap-2.5 grow w-0">
+                <div class="flex flex-col gap-2.5 overflow-auto">
+                    <RawTopic v-for="topic in currentCategory.data.topics" :topic="topic"/>
+                </div>
+
+                <div v-if="currentCategory.meta.total > currentCategory.meta.per_page" class="flex justify-center gap-2.5">
+                    <UPagination :page-count="currentCategory.meta.per_page"
+                                 :total="currentCategory.meta.total"
+                                 v-model="page"/>
+
+                    <UInput v-model.number="goToPage" class="w-16" type="number" @keydown.enter="page = goToPage"/>
+                </div>
             </div>
         </div>
     </UContainer>
